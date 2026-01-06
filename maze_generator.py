1604d27c92bb44bca43482c3bce7b761cfb64db2
Sam Haegelsteen <shaegels@fu-r9-p4.s19.be>
2026-01-06 18:39:28 +0100

    reste le fichier d entree et de sortie

--- maze_generator.py
+++ maze_generator.py
@@ -1,41 +1,33 @@
 import random
+import os
+
+
 
 def generate_maze_skeleton(width: int, height: int)-> list:
     first_line = [-1] * (width * 2 + 1)
-    last_line = [-1] * (width * 2 + 1)
-
+    maze = [first_line.copy()]
     middle_line = [1 if i % 2 == 1 else -1 for i in range(width * 2 + 1)]
 
-    maze = []
-    maze.append(first_line)
-
     for i in range(height * 2 - 1):
-        if i % 2 == 0:
-            maze.append(middle_line.copy())
-        else:
-            maze.append(first_line.copy())
+        maze.append(middle_line.copy() if i % 2 == 0 else first_line.copy())
 
-    maze.append(last_line)
+    maze.append(first_line.copy())
+
     nbr = 1
-    for i in range(height * 2 + 1):
-        for j in range(width * 2 + 1):
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
             if maze[i][j] == 1:
                 nbr += 1
                 maze[i][j] = nbr
-
-    # for row in maze:
-    #     print(row)
     return maze
 
 def is_maze_finished(maze: list)-> bool:
     indicator = maze[1][1]
-    for i in range(len(maze)):
-        for j in range(len(maze[0])):
-            if maze[i][j] not in [-1, 0, indicator]:
+    for row in maze:
+        for cell in row:
+            if cell > 0 and cell != indicator:
                 return False
     return True
-
-
 
 def generate_maze(maze: list):
     candidates = []
@@ -46,218 +38,150 @@ def generate_maze(maze: list):
 
     while candidates and not is_maze_finished(maze):
         chosen_i, chosen_j = random.choice(candidates)
-
-        v_haut = maze[chosen_i - 1][chosen_j]
-        v_bas = maze[chosen_i + 1][chosen_j]
-
+        v_haut, v_bas = maze[chosen_i - 1][chosen_j], maze[chosen_i + 1][chosen_j]
         if v_haut > 0 and v_bas > 0 and v_haut != v_bas:
             maze[chosen_i][chosen_j] = 0
             for r in range(len(maze)):
                 for c in range(len(maze[0])):
-                    if maze[r][c] == v_bas:
-                        maze[r][c] = v_haut
+                    if maze[r][c] == v_bas: maze[r][c] = v_haut
 
-        v_gauche = maze[chosen_i][chosen_j - 1]
-        v_droite = maze[chosen_i][chosen_j + 1]
-
+        v_gauche, v_droite = maze[chosen_i][chosen_j - 1], maze[chosen_i][chosen_j + 1]
         if v_gauche > 0 and v_droite > 0 and v_gauche != v_droite:
             maze[chosen_i][chosen_j] = 0
             for r in range(len(maze)):
                 for c in range(len(maze[0])):
-                    if maze[r][c] == v_droite:
-                        maze[r][c] = v_gauche
+                    if maze[r][c] == v_droite: maze[r][c] = v_gauche
 
         candidates.remove((chosen_i, chosen_j))
-
-    # for row in maze:
-    #     print(row)
     return maze
 
 def close_cells(maze: list, cell_number: tuple)-> list:
     i, j = cell_number
-    maze[i-1][j] = -1
-    maze[i+1][j] = -1
-    maze[i-1][j-1] = -1
-    maze[i-1][j+1] = -1
+    for r in range(i-1, i+2):
+        for c in range(j-1, j+2):
+            if 0 <= r < len(maze) and 0 <= c < len(maze[0]):
+                maze[r][c] = -1
     maze[i][j] = -2
     return maze
 
 def add_42_pattern(maze: list)-> list:
-    height = len(maze) // 2
-    width = len(maze[0])// 2
-    if height % 2 == 1:
-        x = len(maze) // 2
-    if height % 2 == 0:
-        x = len(maze) // 2 + 1
-    if width % 2 == 1:
-        y = len(maze[0]) // 2
-    if width % 2 == 0:
-        y = len(maze[0]) // 2 + 1
-    close_cells(maze, (x, y - 2))
-    close_cells(maze, (x, y - 4))
-    close_cells(maze, (x, y - 6))
-    close_cells(maze, (x - 2, y - 6))
-    close_cells(maze, (x - 4, y - 6))
-    close_cells(maze, (x + 2, y - 2))
-    close_cells(maze, (x + 4, y - 2))
-    close_cells(maze, (x + 4, y + 2))
-    close_cells(maze, (x + 2, y + 2))
-    close_cells(maze, (x , y + 2))
-    close_cells(maze, (x + 4, y + 4))
-    close_cells(maze, (x + 4, y + 6))
-    close_cells(maze, (x , y + 4))
-    close_cells(maze, (x, y + 6))
-    close_cells(maze, (x - 2, y + 6))
-    close_cells(maze, (x - 4, y + 6))
-    close_cells(maze, (x - 4, y + 4))
-    close_cells(maze, (x - 4, y + 2))
-
-
+    x, y = len(maze) // 2, len(maze[0]) // 2
+    if x % 2 == 0: x += 1
+    if y % 2 == 0: y += 1
+    offsets = [(0,-2), (0,-4), (0,-6), (-2,-6), (-4,-6), (2,-2), (4,-2), (4,2), (2,2), (0,2), (4,4), (4,6), (0,4), (0,6), (-2,6), (-4,6), (-4,4), (-4,2)]
+    for ox, oy in offsets:
+        close_cells(maze, (x + ox, y + oy))
     return maze
 
+THEMES = [
+    {
+        "wall": "\033[38;5;237m██\033[0m",
+        "sign": "\033[1;31m██\033[0m",
+        "start": "\033[1;36m██\033[0m",
+        "end": "\033[1;33m██\033[0m",
+        "path": "\033[1;32m██\033[0m"
+    },
+    {
+        "wall": "\033[38;5;234m██\033[0m",
+        "sign": "\033[38;5;198m██\033[0m",
+        "start": "\033[38;5;45m██\033[0m",
+        "end": "\033[38;5;214m██\033[0m",
+        "path": "\033[38;5;118m██\033[0m"
+    },
+    {
+        "wall": "\033[38;5;94m██\033[0m",
+        "sign": "\033[38;5;124m██\033[0m",
+        "start": "\033[38;5;193m██\033[0m",
+        "end": "\033[38;5;220m██\033[0m",
+        "path": "\033[38;5;28m██\033[0m"
+    }
+]
 
 
-
-def print_maze_pretty(maze: list):
-    WALL = "\033[38;5;237m██\033[0m"
-    SIGN = "\033[1;31m██\033[0m"  # -2 (Rouge)
-    START = "\033[1;36m██\033[0m" # -3 (Cyan)
-    END = "\033[1;33m██\033[0m"   # -4 (Jaune)
-    PATH = "\033[1;32m██\033[0m"  # -5 (Vert)
+def print_maze(maze: list, theme: dict, show_path: bool):
     EMPTY = "  "
-
     for row in maze:
         line = ""
         for cell in row:
-            if cell == -1:
-                line += WALL
-            elif cell == -2:
-                line += SIGN
-            elif cell == -3:
-                line += START
-            elif cell == -4:
-                line += END
-            elif cell == -5:
-                line += PATH
-            else:
-                line += EMPTY
+            if cell == -1: line += theme["wall"]
+            elif cell == -2: line += theme["sign"]
+            elif cell == -3: line += theme["start"]
+            elif cell == -4: line += theme["end"]
+            elif cell == -5: line += theme["path"] if show_path else EMPTY
+            else: line += EMPTY
         print(line)
 
-
 def find_shortest_path(maze: list, start: tuple, end: tuple)-> list:
-    j, i = start
-    j_finish, i_finish = end
-    j, i = j * 2 + 1, i * 2 + 1
-    j_finish, i_finish = j_finish * 2 + 1, i_finish * 2 + 1
+    sc, sl = start
+    ec, el = end
+    i, j = sl * 2 + 1, sc * 2 + 1
+    fi, fj = el * 2 + 1, ec * 2 + 1
 
-    finished = False
-    count = -10
-    maze[i][j] = -9
-    following_cell = [(i, j)]
+    finished, count = False, -10
+    maze[i][j], following_cell = -9, [(i, j)]
 
     while not finished and following_cell:
         next_cells = []
-        for curr_i, curr_j in following_cell:
-            directions = [(-1, 0, -2, 0), (1, 0, 2, 0), (0, -1, 0, -2), (0, 1, 0, 2)]
-            for di_w, dj_w, di_c, dj_c in directions:
-                wi, wj = curr_i + di_w, curr_j + dj_w
-                ni, nj = curr_i + di_c, curr_j + dj_c
+        for ci, cj in following_cell:
+            for diw, djw, dic, djc in [(-1,0,-2,0), (1,0,2,0), (0,-1,0,-2), (0,1,0,2)]:
+                wi, wj, ni, nj = ci+diw, cj+djw, ci+dic, cj+djc
                 if 0 <= ni < len(maze) and 0 <= nj < len(maze[0]):
                     if maze[wi][wj] == 0 and maze[ni][nj] > 0:
                         maze[ni][nj] = count
                         next_cells.append((ni, nj))
-                        if ni == i_finish and nj == j_finish:
-                            finished = True
-        following_cell = next_cells
-        count -= 1
+                        if ni == fi and nj == fj: finished = True
+        following_cell, count = next_cells, count - 1
 
     if finished:
-        curr_i, curr_j = i_finish, j_finish
-        while maze[curr_i][curr_j] != -9:
-            val_actuelle = maze[curr_i][curr_j]
-
-            for di_w, dj_w, di_c, dj_c in [(-1,0,-2,0), (1,0,2,0), (0,-1,0,-2), (0,1,0,2)]:
-                ni, nj = curr_i + di_c, curr_j + dj_c
-                wi, wj = curr_i + di_w, curr_j + dj_w
-
+        ci, cj = fi, fj
+        while maze[ci][cj] != -9:
+            val = maze[ci][cj]
+            for diw, djw, dic, djc in [(-1,0,-2,0), (1,0,2,0), (0,-1,0,-2), (0,1,0,2)]:
+                wi, wj, ni, nj = ci+diw, cj+djw, ci+dic, cj+djc
                 if 0 <= ni < len(maze) and 0 <= nj < len(maze[0]):
-                    if (maze[ni][nj] == val_actuelle + 1 or maze[ni][nj] == -9) and maze[wi][wj] == 0:
-                        maze[curr_i][curr_j] = -5
+                    if (maze[ni][nj] == val + 1 or maze[ni][nj] == -9) and maze[wi][wj] == 0:
+                        maze[ci][cj] = -5
                         maze[wi][wj] = -5
-                        curr_i, curr_j = ni, nj
+                        ci, cj = ni, nj
                         break
-
-        maze[i][j] = -3
-        maze[i_finish][j_finish] = -4
-
+        maze[i][j], maze[fi][fj] = -3, -4
     return maze
-
-def print_maze_without_path(maze: list):
-    WALL = "\033[38;5;237m██\033[0m"
-    SIGN = "\033[1;31m██\033[0m"  # -2 (Rouge)
-    START = "\033[1;36m██\033[0m" # -3 (Cyan)
-    END = "\033[1;33m██\033[0m"   # -4 (Jaune)
-    EMPTY = "  "
-
-    for row in maze:
-        line = ""
-        for cell in row:
-            if cell == -1:
-                line += WALL
-            elif cell == -2:
-                line += SIGN
-            elif cell == -3:
-                line += START
-            elif cell == -4:
-                line += END
-            else:
-                line += EMPTY
-        print(line)
-
-
-
 
 
 if __name__ == "__main__":
-    maze = generate_maze_skeleton(20, 20)
-    maze_42 = add_42_pattern(maze)
-    maze_finished = generate_maze(maze)
-    maze_solved = find_shortest_path(maze, (0, 0), (19, 19))
-    print_maze_pretty(maze)
-    user = [0]
-    show_path = True
-    while user is not 4:
+    dim = 15
+    maze = generate_maze_skeleton(dim, dim)
+    add_42_pattern(maze)
+    generate_maze(maze)
+    find_shortest_path(maze, (0, 0), (14, 14))
 
-        print("=== A-Maze-ing ===")
+    user_choice = 0
+    show_path = True
+    current_theme_idx = 0
+
+    while user_choice != 4:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print_maze(maze, THEMES[current_theme_idx], show_path)
+
+        print("\n=== A-Maze-ing ===")
         print("1. Re-generate a new maze")
-        print("2. Show/Hide path from entry to exit")
+        print("2. Show/Hide path")
         print("3. Rotate maze colors")
         print("4. Quit")
-        print("Choice? (1-4):")
+
         try:
-            user = int(input())
+            user_choice = int(input("Choice? (1-4): "))
         except ValueError:
-            user.append(0)
-        print(user)
-        possible_input = [1, 2, 3, 4]
-        if user not in possible_input:
-            print("invalid input")
-        if user == 4:
+            user_choice = 0
+
+        if user_choice == 1:
+            maze = generate_maze_skeleton(dim, dim)
+            add_42_pattern(maze)
+            generate_maze(maze)
+            find_shortest_path(maze, (0, 0), (dim-1, dim-1))
+        elif user_choice == 2:
+            show_path = not show_path
+        elif user_choice == 3:
+            current_theme_idx = (current_theme_idx + 1) % 3
+        elif user_choice == 4:
             quit()
-        if user == 1:
-                maze = generate_maze_skeleton(20, 20)
-                maze_42 = add_42_pattern(maze)
-                maze_finished = generate_maze(maze)
-                maze_solved = find_shortest_path(maze, (0, 0), (19, 19))
-                print_maze_pretty(maze)
-        if user == 2:
-            if show_path == True:
-                print_maze_without_path(maze)
-                show_path = False
-            else:
-                print_maze_pretty(maze)
-                show_path = True
-        print(user)
-
-
-
